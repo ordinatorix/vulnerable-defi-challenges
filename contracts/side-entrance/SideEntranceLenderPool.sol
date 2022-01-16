6bbcf1e611b2a487b194cd9ee9a6856b3c88961a
blue <8978497+seb3n@users.noreply.github.com>
2022-01-15 20:30:31 -0500

    +comments

--- contracts/side-entrance/SideEntranceLenderPool.sol
+++ contracts/side-entrance/SideEntranceLenderPool.sol
@@ -14,25 +14,27 @@ interface IFlashLoanEtherReceiver {
 contract SideEntranceLenderPool {
     using Address for address payable;
 
-    mapping (address => uint256) private balances;
+    mapping(address => uint256) private balances;
 
     function deposit() external payable {
-        balances[msg.sender] += msg.value;
+        balances[msg.sender] += msg.value; // increase msg.sender balance by msg.value
     }
 
     function withdraw() external {
-        uint256 amountToWithdraw = balances[msg.sender];
-        balances[msg.sender] = 0;
-        payable(msg.sender).sendValue(amountToWithdraw);
+        uint256 amountToWithdraw = balances[msg.sender]; // check balance of msg.sender
+        balances[msg.sender] = 0; //update msg.sender balance
+        payable(msg.sender).sendValue(amountToWithdraw); // send eth to msg.sender
     }
 
     function flashLoan(uint256 amount) external {
-        uint256 balanceBefore = address(this).balance;
-        require(balanceBefore >= amount, "Not enough ETH in balance");
-        
+        uint256 balanceBefore = address(this).balance; // check alance of pool
+        require(balanceBefore >= amount, "Not enough ETH in balance"); // check against borrow amount
+
         IFlashLoanEtherReceiver(msg.sender).execute{value: amount}();
 
-        require(address(this).balance >= balanceBefore, "Flash loan hasn't been paid back");        
+        require(
+            address(this).balance >= balanceBefore,
+            "Flash loan hasn't been paid back"
+        ); // required that the balance borrowed is repayed.
     }
 }
- 
